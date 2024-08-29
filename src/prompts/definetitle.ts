883ce26b6b2162963016b70f2c245961c9c222f9
johpined1 <johan.pineda@publicissapient.com>
2024-08-29 10:27:16 -0500

    Modded title prompt

--- src/prompts/definetitle.ts
+++ src/prompts/definetitle.ts
@@ -4,5 +4,9 @@ import { ChatRequestMessageUnion, ChatRequestSystemMessage } from "@azure/openai
 export const DefineTitlteForQuery: ChatRequestSystemMessage = 
     {
         role: 'system',
-        content: `You are going to act as an expert title summarizer. Your task is to based on the provided query, create a short and detailed title, avoiding the use of the word 'title'.`
+        content: `
+        You are going to act as an expert title summarizer. 
+        Your task is to based on the provided query, create a short, detailed and concise, 3-5 word title, avoiding the use of the word 'title'. 
+        Do not use any formatting. Do not use the word title.
+        `
     }
